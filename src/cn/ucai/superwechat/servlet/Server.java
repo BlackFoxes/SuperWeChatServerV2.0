2d723ead6dd9f2566a652a77fab96f504b82b4ca
MyCloudream <chenjun_work@163.com>
2016-07-07 15:50:23 +0800

    添加了若干接口

--- src/cn/ucai/superwechat/servlet/Server.java
+++ src/cn/ucai/superwechat/servlet/Server.java
@@ -32,26 +32,6 @@ public class Server extends HttpServlet {
 		String strReq = request.getParameter("request");
 		System.out.println("strReq:"+strReq);
 		switch (strReq) {
-		case I.REQUEST_FIND_GROUP_BY_USER_NAME:
-			// 26、根据用户名，下载指定下载指定用户的群组列表
-			findAllGroupByUserName(request,response);
-			break;
-		case I.REQUEST_FIND_PUBLIC_GROUPS:
-			// 27、分页下载全部的公开群
-			findPublicGroups(request,response);
-			break;
-		case I.REQUEST_FIND_GROUP_BY_GROUP_NAME:
-			// 28、根据群组名称，模糊查询全部群组列表
-			findGroupByGroupName(request,response);
-			break;
-		case I.REQUEST_FIND_GROUP_BY_ID:
-			// 29、根据群组ID查询群组信息
-			findGroupByGroupId(request,response);
-			break;
-		case I.REQUEST_FIND_GROUP_BY_HXID:
-			// 30、根据环信ID查询群组信息
-			findGroupByHxId(request,response);
-			break;
 		case I.REQUEST_UPLOAD_LOCATION:
 			// 31、上传用户地理位置
 			uploadLocation(request, response);
@@ -115,52 +95,9 @@ public class Server extends HttpServlet {
 		JsonUtil.writeJsonToClient(result, response);
 	}
 
-	/**
-	 * 根据群组名称，模糊查询群组信息
-	 * @param request
-	 * @param response
-	 */
-	private void findGroupByGroupName(HttpServletRequest request, HttpServletResponse response) {
-		String groupName = request.getParameter(I.Group.NAME);
-		Result result = biz.findGroupByGroupName(groupName);
-		JsonUtil.writeJsonToClient(result, response);
-	}
-
-	/**
-	 * 查找所有的公开群
-	 * @param request
-	 * @param response
-	 */
-	private void findPublicGroups(HttpServletRequest request, HttpServletResponse response) {
-		String userName = request.getParameter(I.User.USER_NAME);
-		int pageId = Integer.parseInt(request.getParameter(I.PAGE_ID));
-		int pageSize = Integer.parseInt(request.getParameter(I.PAGE_SIZE));
-		System.out.println("findPublicGroups pageid="+pageId+",pageSize="+pageSize);
-		Result result = biz.findPublicGroups(userName, pageId, pageSize);
-		JsonUtil.writeJsonToClient(result, response);
-	}
-
-	private void findAllGroupByUserName(HttpServletRequest request, HttpServletResponse response) {
-		String userName = request.getParameter(I.User.USER_NAME);
-		Result result = biz.findAllGroupByUserName(userName);
-		JsonUtil.writeJsonToClient(result, response);
-	}
-
-	private void findGroupByHxId(HttpServletRequest request, HttpServletResponse response) {
-		String hxId = request.getParameter(I.Group.HX_ID);
-		Result result = biz.findGroupByHxId(hxId);
-		JsonUtil.writeJsonToClient(result, response);
-	}
-
 	private void findPublicGroupByHxId(HttpServletRequest request, HttpServletResponse response) {
 		String hxId = request.getParameter(I.Group.HX_ID);
 		Result result = biz.findPublicGroupByHxId(hxId);
-		JsonUtil.writeJsonToClient(result, response);
-	}
-
-	private void findGroupByGroupId(HttpServletRequest request, HttpServletResponse response) {
-		String groupId = request.getParameter(I.Group.GROUP_ID);
-		Result result = biz.findGroupByGroupId(groupId);
 		JsonUtil.writeJsonToClient(result, response);
 	}
 }
